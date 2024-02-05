f19fdde6c5504f30dc10fb20908a774df98d8c36
Shivam Sharma <emshivam01@gmail.com>
2024-02-05 23:57:00 +0530

    styling the hero section

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,14 +2,16 @@ import netflixLogo from "../assets/netflix-logo.png";
 
 const Header = () => {
   return (
-    <div className="flex justify-between items-center px-72 ">
-      <img src={netflixLogo} alt="Netflix Logo" className="h-14" />
+    <div className="relative z-10">
+      <div className="absolute flex justify-between items-center w-full px-72">
+        <img src={netflixLogo} alt="Netflix Logo" className="h-20" />
 
-      <div className="">
-        <button className="bg-[#c11119] text-white px-4 py-1 rounded-md shadow-lg">
-          {" "}
-          Sign in
-        </button>
+        <div className="">
+          <button className="bg-[#c11119] text-white text-sm font-medium px-4 py-1 rounded-md shadow-lg">
+            {" "}
+            Sign in
+          </button>
+        </div>
       </div>
     </div>
   );
