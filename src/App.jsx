f12bfc5edfb7ce11a2363cb627524bf739471a2e
Shivam Sharma <emshivam01@gmail.com>
2024-04-23 21:54:46 +0530

    create store and userSlice

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,18 @@ import Header from "./Components/Header";
 
 import { initializeApp } from "firebase/app";
 import { firebaseConfig } from "./utils/firebase"; // Your Firebase configuration
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
 
 // Initialize Firebase outside of the component function
 const firebaseApp = initializeApp(firebaseConfig);
 
 function App() {
   return (
-    <>
+    <Provider store={appStore}>
       <Header />
       <Body />
-    </>
+    </Provider>
   );
 }
 
